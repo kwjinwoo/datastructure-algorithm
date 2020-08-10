dd09acae907c0e913ebe2d82c5f12fce6a1b4e52
kwjinwoo <38836328+kwjinwoo@users.noreply.github.com>
2020-08-10 20:42:57 +0900

    20200810
    20200810

---
+++ src/chap5/RecurX1.java
@@ -0,0 +1,22 @@
+package chap5;
+
+import java.util.Scanner;
+
+public class RecurX1 {
+	static void recur(int n) {
+		while (n > 0) {
+			recur(n - 1);
+			System.out.println(n);
+			n = n - 2;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("정수를 입력하세요 : ");
+		int x = sc.nextInt();
+		
+		recur(x);
+	}
+
+}
